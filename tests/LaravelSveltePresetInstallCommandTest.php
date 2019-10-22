86cda1eea691c8db47636311d2fa9829b5c18e48
Gal Jakic (We Wow Web) <gal.jakic@gmail.com>
2019-10-22 20:00:48 +0200

    Add LaravelSveltePresetInstallCommandTest

---
+++ tests/LaravelSveltePresetInstallCommandTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Wewowweb\LaravelSveltePreset\Tests;
+
+use Orchestra\Testbench\TestCase;
+use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\File;
+use Wewowweb\LaravelSveltePreset\LaravelSveltePresetServiceProvider;
+
+class LaravelSveltePresetInstallCommandTest extends TestCase 
+{
+    /** Overrides the getPackageProviders
+     *  to load the custom package service provider.
+     *  for Orchestra Testbench */
+    protected function getPackageProviders($app)
+    {
+        return [
+            LaravelSveltePresetServiceProvider::class,
+         ];
+    }
+
+    /** @test */
+    public function test_console_install_command()
+    {
+        $this->withoutMockingConsoleOutput();
+
+        $this->artisan('preset svelte');
+
+        $this->assertFileExists(resource_path('js/components/App.svelte'));
+    }
+
+}
+
