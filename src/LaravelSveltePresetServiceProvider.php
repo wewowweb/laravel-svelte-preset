77611807b95b25053e25e3524c819de647cd34a7
Gal Jakic (We Wow Web) <gal.jakic@gmail.com>
2020-03-04 17:59:28 +0100

    Fix PresetCommand dependency issue

--- src/LaravelSveltePresetServiceProvider.php
+++ src/LaravelSveltePresetServiceProvider.php
@@ -2,7 +2,7 @@
 
 namespace Wewowweb\LaravelSveltePreset;
 
-use Illuminate\Foundation\Console\PresetCommand;
+use Laravel\Ui\UiCommand;
 use Illuminate\Support\ServiceProvider;
 
 class LaravelSveltePresetServiceProvider extends ServiceProvider
@@ -12,7 +12,7 @@ class LaravelSveltePresetServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        PresetCommand::macro('svelte', function ($command) {
+        UiCommand::macro('svelte', function ($command) {
             SveltePreset::install();
             $command->info('Svelte scaffolding installed successfully.');
             $command->comment('Please run "npm install && npm run dev" to compile your fresh scaffolding.');
