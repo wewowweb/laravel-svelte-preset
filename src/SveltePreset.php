65514fd65b25a05693b57846682f4eb12ca43acf
Gal Jakic (We Wow Web) <gal.jakic@gmail.com>
2021-11-10 16:51:30 +0100

    Update SveltePreset.php

--- src/SveltePreset.php
+++ src/SveltePreset.php
@@ -19,28 +19,31 @@ class SveltePreset extends Preset
 
     public static function updatePackageArray($packages)
     {
-        return array_merge([
-            'svelte' => '^3.1.0',
-            'laravel-mix-svelte' => '^0.1.0',
-            'svelte-loader' => '^2.13.4', ],
-        Arr::except($packages, [
-            '@babel/preset-react',
-            'react',
-            'react-dom',
-            'vue',
-            'vue-template-compiler',
-        ]));
+        return array_merge(
+            [
+                'svelte' => '^3.1.0',
+                'laravel-mix-svelte' => '^0.5.0',
+                'svelte-loader' => '^2.13.4',
+            ],
+            Arr::except($packages, [
+                '@babel/preset-react',
+                'react',
+                'react-dom',
+                'vue',
+                'vue-template-compiler',
+            ])
+        );
     }
 
     public static function updateMix()
     {
-        copy(__DIR__.'/stubs/webpack.mix.js', base_path('webpack.mix.js'));
+        copy(__DIR__ . '/stubs/webpack.mix.js', base_path('webpack.mix.js'));
     }
 
     public static function updateScripts()
     {
-        copy(__DIR__.'/stubs/app.js', resource_path('js/app.js'));
+        copy(__DIR__ . '/stubs/app.js', resource_path('js/app.js'));
         File::cleanDirectory(resource_path('js/components'));
-        copy(__DIR__.'/stubs/App.svelte', resource_path('js/components/App.svelte'));
+        copy(__DIR__ . '/stubs/App.svelte', resource_path('js/components/App.svelte'));
     }
 }
